834658ac3c8f88542ba15c679d36e0e3e82d52b4
andreamurillomtz <andreamurillomtz@users.noreply.github.com>
2023-12-07 23:20:22 +0000

    published by GitHub Actions

---
+++ schedule/schedule-info.js
@@ -0,0 +1,15 @@
+moreaEventData = [
+  {title: "ASM Charts", url: "/EE260/modules/ASM_Charts", start: "2021-07-12", end: "2021-07-16"},
+  {title: "ASM Charts", url: "/EE260/morea/ASM_Charts/assessment-asm-charts.html", start: "2021-07-16T09:00"},
+  {title: "Min and max terms", url: "/EE260/morea/Min_Max_Terms/assessment-min-and-max-terms.html", start: "2021-07-16T09:00"},
+  {title: "Min Max Terms", url: "/EE260/modules/Min_Max_Terms", start: "4.3.1", end: "4.3.5"},
+  {title: "Logic Gates", url: "/EE260/morea/Logic_Gates/assessment-logic-gates.html", start: "2021-07-16T09:00"},
+  {title: "Logic Gates", url: "/EE260/modules/Logic_Gates", start: "3.1.1", end: "3.1.9"},
+  {title: "Truth Tables", url: "/EE260/morea/Truth_Tables/assessment-truth-tables.html", start: "2021-07-16T09:00"},
+  {title: "Truth Tables", url: "/EE260/modules/Truth-Tables", start: "4.2.1", end: "4.2.3"},
+  {title: "Verilog Quiz 1", url: "/EE260/morea/Verilog/assessment-verilog-quiz1.html", start: "2023-08-31"},
+  {title: "Verilog Basics", url: "/EE260/modules/Verilog", start: "2023-08-01", end: "2023-09-07"},
+  {title: "Verilog Project 1", url: "/EE260/morea/Verilog/assessment-verilog-project1.html", start: "2023-08-31"},
+  {title: "Verilog Lab 1", url: "/EE260/morea/Verilog/experience-verilog-lab1.html", start: "2023-08-31"},
+  {title: "Verilog Lab 2", url: "/EE260/morea/Verilog/experience-verilog-lab2.html", start: "2023-09-07"}
+]
